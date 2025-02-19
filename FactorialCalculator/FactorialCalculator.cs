2aee50a1e4495d54602dc7809277f21bb33b96fa
lbrooks81 <143453730+lbrooks81@users.noreply.github.com>
2025-02-19 15:44:14 -0500

    Dun

--- FactorialCalculator/FactorialCalculator.cs
+++ FactorialCalculator/FactorialCalculator.cs
@@ -2,5 +2,37 @@
 
 public class FactorialCalculator
 {
-    public static void Main() { }
+    public static void Main() 
+    {
+        Console.WriteLine(CalculateFactorial(7));
+    }
+    public static int CalculateFactorial(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(number), number, "Cannot calculate the factorial values of negative numbers.");
+        }
+        return Factorial(number, number);
+    }
+    private static int Factorial(int number, int total)
+    {
+        if (total == 0)
+        {
+            return 1;
+        }
+        
+        // Base Case
+        if (number == 1)
+        {
+            return total;
+        }
+
+        checked
+        {
+            number--;
+            total *= number;
+        }
+
+        return Factorial(number, total);
+    }
 }
---
+++ FactorialTests/FactorialTests.cs
@@ -0,0 +1,54 @@
+﻿namespace FactorialTests
+{
+    [TestClass]
+    public sealed class FactorialTests
+    {
+        [TestMethod]
+        public void Factorial_WhenNumberIsZero_ReturnsOne()
+        {
+            // Arrange
+            int number = 0;
+            int expectedFactorial = 1;
+            
+            // Act
+            int factorial = FactorialCalculator.FactorialCalculator.CalculateFactorial(number);
+
+            // Assert
+            Assert.AreEqual(expectedFactorial, factorial);
+        }
+        
+        [TestMethod]
+        public void Factorial_NumberIsValidAndPositive_ReturnsFactorial()
+        {
+            // Arrange
+            int number = 7;
+            int expectedFactorial = 5040;
+
+            // Act
+            int factorial = FactorialCalculator.FactorialCalculator.CalculateFactorial(number);
+
+            // Assert
+            Assert.AreEqual(expectedFactorial, factorial);
+        }
+
+        [TestMethod]
+        public void Factorial_WhenNumberIsNegative_ThrowsOutOfRangeException()
+        {
+            // Arrange
+            int number = -2;
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => FactorialCalculator.FactorialCalculator.CalculateFactorial(number));
+        }
+
+        [TestMethod]
+        public void Factorial_WhenNumberIsTooHigh_ThrowsOverflowException()
+        {
+            // Arrange
+            int number = int.MaxValue;
+
+            // Act and Assert
+            Assert.ThrowsException<OverflowException>(() => FactorialCalculator.FactorialCalculator.CalculateFactorial(number));
+        }
+    }
+}
